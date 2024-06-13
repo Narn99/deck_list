719f7bf3e696c17dbfc340ef3e0951b9ee835a5e
Narn99 <vkdldjtmflr@naver.com>
2024-06-13 20:15:34 +0900

    레어도를 css가 아니라 컴포넌트로 변경

---
+++ deck_list/src/CSS/Rare.tsx
@@ -0,0 +1,15 @@
+/* 
+ 시크릿, 울레, 슈레, 레어, 테두리까지인 시크릿 반짝반짝
+ 근데 이름 칸은 반짝이게 하기가 힘들잖아...
+
+ 레어도별 컴포넌트를 만들어서 export 하자
+
+ 모달 */
+
+import React from "react";
+
+const Rare = () => {
+  return <div>Rare</div>;
+};
+
+export default Rare;
