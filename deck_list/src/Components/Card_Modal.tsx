f4f7c605f4437e6db32e513440587e300d0cf33f
Narn99 <vkdldjtmflr@naver.com>
2024-06-14 20:51:38 +0900

    카드 클릭 시 뜨는 모달창 제작

--- deck_list/src/Components/Card_Modal.tsx
+++ deck_list/src/Components/Card_Modal.tsx
@@ -1,7 +1,32 @@
 // 카드를 클릭 시, 확대해서 보여주는 모달
 
+import styled from "@emotion/styled";
+
+const ModalContainer = styled("div")`
+  width: 50vw;
+
+  z-index: 5000;
+`;
+
+const ModalBackground = styled("div")`
+  position: absolute;
+
+  background-color: black;
+  opacity: 0.4;
+
+  width: 100vw;
+  height: 100vh;
+
+  z-index: 4000;
+`;
+
 const Card_Modal = () => {
-  return <div>Card_Modal</div>;
+  return (
+    <>
+      <ModalContainer></ModalContainer>
+      <ModalBackground />
+    </>
+  );
 };
 
 export default Card_Modal;
